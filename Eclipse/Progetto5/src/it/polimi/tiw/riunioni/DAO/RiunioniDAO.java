c5749844890e4588622550732f064783804d0c3c
lotta98 <57574060+lotta98@users.noreply.github.com>
2020-04-05 12:01:52 +0200

    RiunioneDao
    RiunioneDao

--- Eclipse/Progetto5/src/it/polimi/tiw/riunioni/DAO/RiunioniDAO.java
+++ Eclipse/Progetto5/src/it/polimi/tiw/riunioni/DAO/RiunioniDAO.java
@@ -1,11 +1,111 @@
 package it.polimi.tiw.riunioni.DAO;
 
-import java.util.Date;
+import java.io.InputStream;
+import it.polimi.tiw.posts.beans.Post;
+import it.polimi.tiw.riunioni.beans.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;;
 import java.util.TimeZone;
+import java.util.List;
+
 
 public class RiunioniDAO {
-	private int id;
-	private String titolo;
-	private Date data;
-	private TimeZone ora;
+	private Connection con;
+
+	public RiunioniDAO (Connection connection) {
+		this.con = connection;
+	}
+
+
+	public List<Riunione> findRiunioniByUser(int idUtente) throws SQLException {
+
+		List<Riunione> riunioni = new ArrayList<Riunione>();
+		String query = "SELECT * FROM riunione_partecipanti WHERE idPart = ?";
+		ResultSet result = null;
+		PreparedStatement pstatement = null;
+		try {
+			pstatement = con.prepareStatement(query);
+			pstatement.setInt(1, idUtente);
+			result = pstatement.executeQuery();
+			while (result.next()) {
+				Riunione riunione = new Riunione();
+
+				riunione.setId(result.getId("id"));
+				riunione.setTitolo(result.getString("titolo"));
+
+				riunione.setData(result.getData("data"));
+				riunione.setOra(result.getOra("ora"));
+
+				riunione.setMaxPart(result.getMaxPart("maxPart"));
+				riunione.setIdCreatore(result.getIdCreatore("idCreatore"));
+
+				riunioni.add(riunione);
+			}
+		}  catch (SQLException e) {
+			throw new SQLException(e);
+		}finally {
+			try {
+				result.close();
+			} catch (Exception e1) {
+				throw new SQLException("Cannot close result");
+			}
+			try {
+				pstatement.close();
+			} catch (Exception e1) {
+				throw new SQLException("Cannot close statement");
+			}
+		}
+		return riunioni;
+	}
+
+	public List<Riunione> findRiunioniCreate(int idUtente) throws SQLException {
+
+		List<Riunione> riunioni = new ArrayList<Riunione>();
+		String query = "SELECT * FROM riunione WHERE idCreatore = ?";
+		ResultSet result = null;
+		PreparedStatement pstatement = null;
+		try {
+			pstatement = con.prepareStatement(query);
+			pstatement.setInt(1, idUtente);
+			result = pstatement.executeQuery();
+			while (result.next()) {
+				Riunione riunione = new Riunione();
+
+				riunione.setId(result.getId("id"));
+				riunione.setTitolo(result.getString("titolo"));
+
+				riunione.setData(result.getData("data"));
+				riunione.setOra(result.getOra("ora"));
+
+				riunione.setMaxPart(result.getMaxPart("maxPart"));
+				riunione.setIdCreatore(result.getIdCreatore("idCreatore"));
+
+				riunioni.add(riunione);
+			}
+
+		} catch (SQLException e) {
+			throw new SQLException(e);
+
+		}finally {
+			try {
+				result.close();
+			} catch (Exception e1) {
+				throw new SQLException("Cannot close result");
+			}
+			try {
+				pstatement.close();
+			} catch (Exception e1) {
+				throw new SQLException("Cannot close statement");
+			}
+		}
+		return riunioni; 
+	}
 }
+
+
+
---
+++ Eclipse/Progetto5/src/it/polimi/tiw/riunioni/controllers/GoToHomePage.java
@@ -0,0 +1,5 @@
+package it.polimi.tiw.riunioni.controllers;
+
+public class GoToHomePage {
+
+}
