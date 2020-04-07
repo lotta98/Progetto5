5b3a6026a844895abcb4649b2a6482ab74e97a8a
lowinmhm98 <58476638+lowinmhm98@users.noreply.github.com>
2020-04-07 15:38:09 +0200

    Update CheckPartecipanti.java

--- Eclipse/Progetto5/src/it/polimi/tiw/riunioni/controllers/CheckPartecipanti.java
+++ Eclipse/Progetto5/src/it/polimi/tiw/riunioni/controllers/CheckPartecipanti.java
@@ -1,5 +1,45 @@
 package it.polimi.tiw.riunioni.controllers;
+import  javax.servlet.*;
+import java.io.*;
+import it.polimi.tiw.riunioni.DAO.*;
+import it.polimi.tiw.riunioni.beans.*;
 
-public class CheckPartecipanti {
 
+public class CheckPartecipanti extends HttpServlet {
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	
+		
+		HttpSession session = request.getSession();
+		if(session.isNew())
+		session.setAttribute("counter",1);
+		else 
+		{ int toAdd= (int)session.getAttribute("counter")+1;
+		session.setAttribute("counter",toAdd); // update counterfor thissession
+		}
+		
+		
+	
+	if (/*numeroutenti*/ > request.getAttribute(RiunioneDaCreare).getMaxPart() && counter <=3) {
+		
+		String pathhome = getServletContext().getContextPath() + "/homepage.jsp";
+	response.sendRedirect(pathhome);
+	return ;
+	} else if (/*numeroutenti*/ > request.getAttribute(RiunioneDaCreare).getMaxPart() && counter >3) {
+		String pathend = getServletContext().getContextPath() + "/PaginaCancellazione.jsp";
+		response.sendRedirect(pathend);
+		return;
+	} else {
+		
+	   addRiunione(request);
+		addRiunionePartecipanti(request.getAttribute(RiunioneDaCreare).getId(),request);
+		
+		
+		response.sendRedirect("/GotoAnagrPage");
+		return;
+	}
+		
 }
