9d881238f6fa47ee93500fa226f6b97ed008c56c
Emilio <emilio.delorenzis@mail.polimi.it>
2020-04-10 15:03:33 +0200

    Update Registrazione.java

--- Eclipse/Progetto5/src/it/polimi/tiw/riunioni/controllers/Registrazione.java
+++ Eclipse/Progetto5/src/it/polimi/tiw/riunioni/controllers/Registrazione.java
@@ -15,6 +15,7 @@ import java.sql.SQLException;
 public class Registrazione extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	private Connection connection = null;
+	
 	public void init() throws ServletException {
 		try {
 			ServletContext context = getServletContext();
@@ -34,38 +35,44 @@ public class Registrazione extends HttpServlet{
 		}
 	}
 	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request,response);
+	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String usrn = request.getParameter("username");
 		String pwd = request.getParameter("password");
-		String pwdconf = request.getParameter("password");
+		String pwdconf = request.getParameter("password2");
 		String nome = request.getParameter("nome");
 		String cognome = request.getParameter("cognome");
 		UtenteDAO usr = new UtenteDAO(connection);
-		Utente u = null;
-		try {
-			u = usr.checkUser(usrn, pwd);
-		} catch (SQLException e) {
-			
-			response.sendError(HttpServletResponse.SC_BAD_GATEWAY, "Failure in database credential checking");
- 		}
+		Utente u = new Utente();
 		String path = getServletContext().getContextPath();
-		if (u == null && pwd==pwdconf) {
-			 
-			u.setUsername(usrn);
+		
+		if (pwd.equals(pwdconf)) {
+			
+			u.setUser(usrn);
 			u.setPassword(pwd);
 			u.setNome(nome);
 			u.setCognome(cognome);
-			usr.addUtente(u);
+			try {
+				usr.addUtente(u);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			path = getServletContext().getContextPath() + "/login.jsp";
 			response.sendRedirect(path);
 			
-		} else {
-			  patherror = getServletContext().getContextPath() + "/Registrazione.jsp";
-			response.sendRedirect(patherror);
-			
-		}
+		} 
+		else {
+		 String patherror = getServletContext().getContextPath() + "/Registrazione.jsp";
+		response.sendRedirect(patherror);
 		
+	}
+	
 	}
 	
 	
@@ -81,4 +88,4 @@ public class Registrazione extends HttpServlet{
 
 	
 	
-}
+
